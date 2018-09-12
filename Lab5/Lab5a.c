10a0f48f97a8595245144a9c585a5026ede31543
Jonathan Lim <jonathanlcy@gmail.com>
2018-09-11 19:08:34 -0700

    Add files via upload

---
+++ Lab5/Lab5a.c
@@ -0,0 +1,40 @@
+/*	
+	EECS20_Lab5a.c
+	Spring 2018
+	Jonathan Lim
+*/
+
+#include <stdio.h>
+#include <assert.h>
+
+int main(void) {
+	int a, b, c, d;
+	int sum, product, diff, quotient, remainder;
+
+	printf("Hello! Please enter the first non-zero integer number for calculations: ");
+	scanf("%d", &a);
+	assert(a != 0);
+	printf("Please enter the second non-zero integer number for calculations: ");
+	scanf("%d", &b);
+	assert(b != 0);
+	printf("Please enter the third non-zero integer number for calculations: ");
+	scanf("%d", &c);
+	assert(c != 0);
+	printf("Please enter the fourth non-zero integer number for calculations: ");
+	scanf("%d", &d);
+	assert(d != 0);
+
+	//The calculations are done below
+	sum = a + b + c + d;
+	product = a * b * c * d;
+	diff = a + b - c - d;
+	quotient = (a + b) / (c + d);
+	remainder = (a + b + c) % d;
+	printf("The sum of the four integers is: %d\n", sum);
+	printf("The product of the four integers is: %d\n", product);
+	printf("The diff of the four integers is: %d\n", diff);
+	printf("The quotient of the four integers is: %d\n", quotient);
+	printf("The remainder of the four integers is: %d\n", remainder);
+
+	return 0;
+}
---
+++ Lab5/Lab5b.c
@@ -0,0 +1,25 @@
+/*	
+	EECS20_Lab5b.c
+	Spring 2018
+	Jonathan Lim
+*/
+
+#include <stdio.h>
+#include <assert.h>
+
+int main() {
+	int counter = 0;
+	char temp;
+	char num[7];
+	printf("Please enter a seven-digit integer number: ");
+	scanf("%s", &num);
+	assert(strlen(num) == 7);
+	while (counter != 4) {
+		temp = num[counter];
+		num[counter] = num[6 - counter];
+		num[6 - counter] = temp;
+		counter++;
+	}
+	printf("The reverse order of the seven-digit integer number is %s", num);
+	return 0;
+}
